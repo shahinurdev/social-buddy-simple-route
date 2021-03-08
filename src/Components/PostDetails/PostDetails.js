93427761744310edf35303f177b1850e908e5a4e
shahinurdev <mdshahinur6@gmail.com>
2021-03-08 13:02:51 +0600

    o

--- src/Components/PostDetails/PostDetails.js
+++ src/Components/PostDetails/PostDetails.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import Comments from '../Comments/Comments';
 
-
 const PostDetails = () => {
     const {id} = useParams();
     const [post,setPost] = useState({});
@@ -23,7 +22,6 @@ const PostDetails = () => {
         <div>
           <h4>{post.id}</h4>
           <h4>{post.title}</h4>
-
           {
               comments.map(comment=> <Comments comment={comment}></Comments> )
           }
